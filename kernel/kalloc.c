5252b952b5e829d1246e5b1cb390b610610b7891
VictorHuu <victorhu493@gmail.com>
2023-07-18 04:33:26 +0800

    Have done the Lab

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -22,11 +22,15 @@ struct {
   struct spinlock lock;
   struct run *freelist;
 } kmem;
-
+struct{
+  struct spinlock lock;
+  int cnt[PHYSTOP/PGSIZE];
+}ref;
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&ref.lock,"ref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +39,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    ref.cnt[(uint64)p/PGSIZE]=1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -50,18 +56,86 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+  acquire(&ref.lock);
+  if(--ref.cnt[(uint64)pa/PGSIZE]==0){
+	release(&ref.lock);
+  	// Fill with junk to catch dangling refs.
+  	memset(pa, 1, PGSIZE);
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  	r = (struct run*)pa;
 
-  r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  	acquire(&kmem.lock);
+  	r->next = kmem.freelist;
+  	kmem.freelist = r;
+  	release(&kmem.lock);
+  }else
+  	release(&ref.lock);
+  
+}
+//Judge whether the very page is a cow page.
+int cowpage(pagetable_t pagetable, uint64 va) {
+  if(va >= MAXVA)
+    return -1;
+  pte_t* pte = walk(pagetable, va, 0);
+  if(pte == 0)
+    return -1;
+  if((*pte & PTE_V) == 0)
+    return -1;
+  return (*pte & PTE_COW ? 0 : -1);
 }
 
+void* cowalloc(pagetable_t pagetable, uint64 va) {
+  uint flags;
+  if(va % PGSIZE != 0)
+    return 0;
+
+  uint64 pa = walkaddr(pagetable, va);  
+  if(pa == 0)
+    return 0;
+
+  pte_t* pte = walk(pagetable, va, 0); 
+
+  if(krefcnt((char*)pa) == 1) {//Only 1 reference
+    *pte |= PTE_W;
+    *pte &= ~PTE_COW;
+    return (void*)pa;
+  } else {//late map a real page for the PTE
+    char* mem = kalloc();
+    if(mem == 0)
+      return 0;
+
+    memmove(mem, (char*)pa, PGSIZE);
+
+    *pte &= ~PTE_V;//avoid remap error
+    flags=PTE_FLAGS(*pte);
+    
+    flags=flags|PTE_W;
+    flags=flags&~PTE_COW;
+    if(mappages(pagetable, va, PGSIZE, (uint64)mem, flags) != 0) {
+      kfree(mem);
+      *pte |= PTE_V;//pair with the above PTE_V disabling.
+      return 0;
+    }
+
+
+    kfree((char*)PGROUNDDOWN(pa));
+    return mem;
+  }
+}
+//Get the reference count of the physical address pa
+int get_refcnt(void* pa) {
+  return ref.cnt[(uint64)pa / PGSIZE];
+}
+
+//Increment the reference count of pa
+int inc_refcnt(void* pa) {
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    return -1;
+  acquire(&ref.lock);
+  ++ref.cnt[(uint64)pa / PGSIZE];
+  release(&ref.lock);
+  return 0;
+}
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
@@ -72,8 +146,12 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
     kmem.freelist = r->next;
+    acquire(&ref.lock);
+    ref.cnt[(uint64)r/PGSIZE]=1;
+    release(&ref.lock);
+  }
   release(&kmem.lock);
 
   if(r)
