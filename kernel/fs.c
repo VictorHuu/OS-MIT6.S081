83e73800f0590f07a7f778b9ee85f5729d8900ef
VictorHuu <victorhu493@gmail.com>
2023-07-23 07:13:43 +0800

    Have done the first lab

--- kernel/fs.c
+++ kernel/fs.c
@@ -400,6 +400,27 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+  bn-=NINDIRECT;
+  if(bn < NINDIRECT*NINDIRECT){
+  	//Load doubly-indirect block,allocating if necessary.
+	if((addr=ip->addrs[NDIRECT+1])==0)
+		ip->addrs[NDIRECT+1]=addr=balloc(ip->dev);
+	bp=bread(ip->dev,addr);
+	a=(uint*)bp->data;
+	if((addr=a[bn/NINDIRECT])==0){
+		a[bn/NINDIRECT]=addr=balloc(ip->dev);
+		log_write(bp);
+	}
+	brelse(bp);
+	bp=bread(ip->dev,addr);
+	a=(uint*)bp->data;
+	if((addr=a[bn%NINDIRECT])==0){
+		a[bn%NINDIRECT]=addr=balloc(ip->dev);
+		log_write(bp);
+	}
+	brelse(bp);
+	return addr;  
+  }
 
   panic("bmap: out of range");
 }
@@ -431,7 +452,28 @@ itrunc(struct inode *ip)
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
   }
-
+  struct buf *bp1;
+  uint *a1;
+  if(ip->addrs[NDIRECT+1]){
+  	bp=bread(ip->dev,ip->addrs[NDIRECT+1]);
+	a=(uint*)bp->data;
+	for(i=0;i<NINDIRECT;i++){
+		if(a[i]){
+			bp1=bread(ip->dev,a[i]);
+			a1=(uint*)bp1->data;
+			for(j=0;j<NINDIRECT;j++){
+				if(a1[j]){
+					bfree(ip->dev,a1[j]);
+				}
+			}
+			brelse(bp1);
+			bfree(ip->dev,a[i]);
+		}
+	}
+	brelse(bp);
+	bfree(ip->dev,ip->addrs[NDIRECT+1]);
+	ip->addrs[NINDIRECT+1]=0;
+  }
   ip->size = 0;
   iupdate(ip);
 }
